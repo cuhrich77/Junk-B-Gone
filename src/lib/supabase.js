234306996102525c255112687a87cadc67f18437
cuhrich77 <cuhrich77@gmail.com>
2026-04-22 18:56:21 -0400

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://pfrwtxznrtohdlblhfbc.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmcnd0eHpucnRvaGRsYmxoZmJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MzY2MTAsImV4cCI6MjA5MjIxMjYxMH0.wfPK2tIrS4o1R17VFNEnRrfEAcX_pdd81xgpO8aDpZc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
